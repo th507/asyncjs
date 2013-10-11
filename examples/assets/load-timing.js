99d5c3ca431d088ce9b342b7e1306ae2816e4263
Jingwei "John" Liu <liujingwei@gmail.com>
2013-10-11 21:54:11 +0800

    less intrusive logging in example pages

--- examples/assets/load-timing.js
+++ examples/assets/load-timing.js
@@ -2,20 +2,20 @@ window.onload = function() {
     try {
         timing = performance.timing;
         dr = timing.domContentLoadedEventEnd - timing.navigationStart;
-        document.body.innerHTML += ("DOMContentReady fires at: " + Date.now());
-        document.body.innerHTML += ", " + timing.domContentLoadedEventEnd + " (Timing API)<br>";
+        log("DOMContentReady fires at: " + Date.now());
+        log(", " + timing.domContentLoadedEventEnd + " (Timing API)");
 
 
-        document.body.innerHTML += "<hr>";
+        log("<hr>");
 
-        document.body.innerHTML += "<strong>Time Elapsed:</strong><br>";
+        log("<strong>Time Elapsed:</strong><br>");
 
-        document.body.innerHTML += ("to domContentLoaded: " + dr + " ms<br>") ;
+        log("to domContentLoaded: " + dr + " ms<br>");
 
         setTimeout(function() {
             try {
                 ld = timing.loadEventEnd - timing.navigationStart;
-                document.body.innerHTML += ("to load: " + ld + " ms<br>");
+                log("to load: " + ld + " ms<br>");
             } catch(e) {}
         }, 200);
     } catch(e) {}
